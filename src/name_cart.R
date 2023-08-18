d0abff1d1161e8af6a9670ec46d94113b54b5d50
mwidas <mel.widas@gmail.com>
2023-08-18 14:00:54 -0700

    update

---
+++ src/name_cart.R
@@ -0,0 +1,14 @@
+#' Make food cart name
+#'
+#' @param food Your favorite food
+#' @param animal Your favorite animal
+#'
+#' @return
+#' @export
+#'
+#' @examples
+name_cart <- function(food, animal) {
+  paste0("Mc", stringr::str_to_title(animal), "'s ", str_to_title(food), "Mart")
+}
+
+#test in console
